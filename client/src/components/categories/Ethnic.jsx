817fecac5c80e94001baba28d3541358a3a9cb2d
GARVIT <118100178+garv2401@users.noreply.github.com>
2024-11-08 12:43:04 +0530

    Update Ethnic.jsx

--- client/src/components/categories/Ethnic.jsx
+++ client/src/components/categories/Ethnic.jsx
@@ -13,7 +13,7 @@ const Ethnic = () => {
   
 
   const getProducts=async()=>{
-      const res=await axios.get('/api/ethnicProducts');
+      const res=await axios.get('https://rivora-server.onrender.com/api/ethnicProducts');
       //console.log(res.data.products);
       setproducts(res.data.products);
       console.log(products);
